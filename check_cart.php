ab49a47ce12e47963ad747e4eb318aea0f62633e
ahdave1573-dev <ahdave1573@gmail.com>
2026-04-05 17:09:47 +0530

    cart validation

---
+++ check_cart.php
@@ -0,0 +1,11 @@
+<?php
+include('config/db.php');
+$res = mysqli_query($conn, "SHOW TABLES LIKE 'cart'");
+if(mysqli_num_rows($res) > 0) {
+    echo "cart table exists\n";
+    $res2 = mysqli_query($conn, "DESCRIBE cart");
+    while($row = mysqli_fetch_assoc($res2)) print_r($row);
+} else {
+    echo "cart table DOES NOT exist\n";
+}
+?>
