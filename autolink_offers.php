951bbaeef9365a8052a74bf0697667053e37bb79
ahdave1573-dev <ahdave1573@gmail.com>
2026-04-06 19:24:29 +0530

    automated offer

---
+++ autolink_offers.php
@@ -0,0 +1,16 @@
+<?php
+include('config/db.php');
+
+$offers = mysqli_query($conn, "SELECT id, title FROM offers WHERE product_id IS NULL OR product_id = 0");
+$count = 0;
+while($offer = mysqli_fetch_assoc($offers)) {
+    $title = mysqli_real_escape_string($conn, $offer['title']);
+    $res = mysqli_query($conn, "SELECT id FROM products WHERE name = '$title' LIMIT 1");
+    if($prod = mysqli_fetch_assoc($res)) {
+        $pid = $prod['id'];
+        mysqli_query($conn, "UPDATE offers SET product_id = $pid WHERE id = " . $offer['id']);
+        $count++;
+    }
+}
+echo "Auto-linked $count offers to products by name.";
+?>
